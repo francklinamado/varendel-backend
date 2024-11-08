de71bb61043aa73af57c08154c66594316311766
francklinamado <alexandrevagabundoamado@gmail.com>
2024-11-08 18:48:28 +0100

    Corrige o caminho do diretório 'static' no settings.py

--- campus7_system/settings.py
+++ campus7_system/settings.py
@@ -1,6 +1,7 @@
 
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -108,6 +109,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Local onde os arquivos serão coletados
+
+# Diretórios onde o Django buscará por arquivos estáticos
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
